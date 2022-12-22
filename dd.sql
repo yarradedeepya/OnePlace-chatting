a734c348009ff02173492e4b955f726be5f729fc
Dedeepya Yarra <dedeepya.dd@gmail.com>
2022-12-22 23:45:34 +0530

    Add files via upload

---
+++ dd.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 07:14 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+-- Error reading structure for table dd.chat: #1932 - Table 'dd.chat' doesn't exist in engine
+-- Error reading data for table dd.chat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `dd`.`chat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+-- Error reading structure for table dd.register: #1932 - Table 'dd.register' doesn't exist in engine
+-- Error reading data for table dd.register: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `dd`.`register`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+-- Error reading structure for table dd.request: #1932 - Table 'dd.request' doesn't exist in engine
+-- Error reading data for table dd.request: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `dd`.`request`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+-- Error reading structure for table dd.rooms: #1932 - Table 'dd.rooms' doesn't exist in engine
+-- Error reading data for table dd.rooms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `dd`.`rooms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rowscount`
+--
+-- Error reading structure for table dd.rowscount: #1932 - Table 'dd.rowscount' doesn't exist in engine
+-- Error reading data for table dd.rowscount: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `dd`.`rowscount`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
